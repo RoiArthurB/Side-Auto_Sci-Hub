b4d591c2952ce77c62c9102ea0adabf9c362fd42
Arthur Brugiere <arbr1@hotmail.fr>
2019-09-25 17:54:14 +0200

    Revert to Browser Action

--- shRedirect.js
+++ shRedirect.js
@@ -1,5 +1,5 @@
 /*
- *	GLOBAL VAR
+ *  GLOBAL VAR
  */
 
 var baseURL = "https://whereisscihub.now.sh/go/";
@@ -7,32 +7,17 @@ var baseURL = "https://whereisscihub.now.sh/go/";
 // Firefox always has both chrome and browser objects, Chrome has only chrome
 var browser = browser || chrome
 
-function logTabs(tab) {
+function openSciHubTab(tab) {
     if ( tab.active ){
         browser.tabs.create({
             url: baseURL + escape(tab.url)
         });
-        console.log(tab);
     }
 }
 
 /*
- *	EVENT LISTENER
+ *  EVENT LISTENER
  */
 
 // Listener
-browser.pageAction.onClicked.addListener(function(tab) { logTabs(tab) });
-
-function checkForValidUrl(tabId, changeInfo, tab)
-    {
-        if(typeof tab != "undefined" && typeof tab != "null" )
-        {
-                // ... show the page action.
-                browser.pageAction.show(tabId);
-        }
-    };
-
-// Listen for any changes to the URL of any tab.
-// Since Chrome does'nt support hide_match or show_match
-browser.tabs.onUpdated.addListener(checkForValidUrl);
-browser.tabs.onActivated.addListener(checkForValidUrl);
+browser.browserAction.onClicked.addListener(function(tab) { openSciHubTab(tab) });
