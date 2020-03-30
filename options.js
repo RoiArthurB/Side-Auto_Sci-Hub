9fc8545d1d9a807416bb3e64c7b1f423cf8fce33
Giacomo Rossetto <jackyman_cs4@live.it>
2020-03-30 15:53:35 +0200

    Improve url validation
    Accepted: 
     - https://example.com/
     - http://example.com/
     - example.com
     - example.com/

--- options.js
+++ options.js
@@ -25,16 +25,48 @@ storage.get(config, function (config) {
   defaultUrlInput.disabled = false;
 });
 
+function buildURL(url) {
+  // Check protocol
+  if (!url.startsWith("https://") && !url.startsWith("http://")) {
+    // if absent, assume is http
+    url = "http://" + url
+  }
+  // Check trailing slash
+  if (!url.endsWith("/")) {
+    url = url + "/"
+  }
+  return url
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/URL/URL
+function isValidUrl (url) {
+  try {
+    new URL(url)
+  } catch (error) {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypeError
+    return false
+  }
+  return true
+}
+
 function registerChanges(e) {
-  if (!e.target.value.match(/^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?\/$/)) {
-    document.getElementById("defaultUrlError").style.display = 'block';
-    document.getElementById("defaultUrlSaved").style.display = 'none';
-  } else {
+  let config = {};
+  if (e.target.value == "") {
     document.getElementById("defaultUrlError").style.display = 'none';
     document.getElementById("defaultUrlSaved").style.display = 'block';
-    let config = {};
-    config[SCIHUB_URL] = e.target.value;
+    config[SCIHUB_URL] = DEFAULT_SCIHUB_URL;
     storage.set(config);
+  } else {
+    let url = buildURL(e.target.value)
+    if (!isValidUrl(url)) {
+      document.getElementById("defaultUrlError").style.display = 'block';
+      document.getElementById("defaultUrlSaved").style.display = 'none';
+    } else {
+      document.getElementById("defaultUrlError").style.display = 'none';
+      document.getElementById("defaultUrlSaved").style.display = 'block';
+      config[SCIHUB_URL] = url;
+      storage.set(config);
+    }
   }
 }
 
